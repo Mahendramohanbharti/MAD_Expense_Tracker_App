33b3c6c36b6243b9ff378346ce5249d33bc6f4d8
Mahendra Mohan Bharti <bhartisiddharth00000@gmail.com>
2020-10-08 23:05:31 +0530

    Connect Custom Dialog

--- app/src/main/java/com/example/mahendramohanbharti/DashBoardFragment.java
+++ app/src/main/java/com/example/mahendramohanbharti/DashBoardFragment.java
@@ -1,17 +1,26 @@
 package com.example.mahendramohanbharti;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.core.DatabaseInfo;
 
 
 public class DashBoardFragment extends Fragment {
@@ -36,12 +45,23 @@ public class DashBoardFragment extends Fragment {
     //Animation
     private Animation FadOpen,FadClose;
 
+    //Firebase
+    private FirebaseAuth mAuth;
+    private DatabaseReference mIncomeDatabase;
+    private DatabaseReference mExpenseDatabase;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View myview= inflater.inflate(R.layout.fragment_dash_board, container, false);
+
+        mAuth=FirebaseAuth.getInstance();
+        FirebaseUser mUser=mAuth.getCurrentUser();
+        String uid=mUser.getUid();
+        mIncomeDatabase= FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
+        mExpenseDatabase=FirebaseDatabase.getInstance().getReference().child("ExpenseDatabase").child(uid);
 
         //connect floating button to layout
         fab_main_btn=myview.findViewById(R.id.fb_main_plus_btn);
@@ -98,15 +118,58 @@ public class DashBoardFragment extends Fragment {
         fab_income_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                incomeDataInsert();
             }
         });
         fab_expense_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
     }
+    public void incomeDataInsert(){
+        AlertDialog.Builder mydialog=new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater=LayoutInflater.from(getActivity());
+        View myviewm=inflater.inflate(R.layout.custom_layout_for_insertdata,null);
+        mydialog.setView(myviewm);
+        final AlertDialog dialog=mydialog.create();
+
+        final EditText edtAmmout= myviewm.findViewById(R.id.ammount_edt);
+        final EditText edtType=myviewm.findViewById(R.id.type_edt);
+        final EditText edtNote=myviewm.findViewById(R.id.note_edt);
+
+        Button btnSave=myviewm.findViewById(R.id.btnSave);
+        Button btnCancel=myviewm.findViewById(R.id.btnCancel);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String type=edtType.getText().toString().trim();
+                String ammount=edtAmmout.getText().toString().trim();
+                String note=edtNote.getText().toString().trim();
+
+                if(TextUtils.isEmpty(type)){
+                    edtType.setError("Required failed...");
+                }
+                if(TextUtils.isEmpty(ammount)){
+                    edtAmmout.setError("Required failed...");
+                }
+                int ourammountint=Integer.parseInt(ammount);
+                if(TextUtils.isEmpty(note)){
+                    edtNote.setError("Required failed...");
+                }
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+
 }
